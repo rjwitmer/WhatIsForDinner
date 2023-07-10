46a6d46ea522060e2b339ac008a0d88197b39225
Bob Witmer <bwitmer@golden.net>
2023-07-10 10:34:24 -0400

    Initial Commit

---
+++ WhatIsForDinner/WhatIsForDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsForDinnerApp.swift
+//  WhatIsForDinner
+//
+//  Created by Bob Witmer on 2023-07-10.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsForDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
